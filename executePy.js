822c4f18340b28bc5cb5634aa45bfef63272d7cc
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2022-07-12 10:35:58 +0530

    new change

--- executePy.js
+++ executePy.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 const executePy = async ({filepath,inputPath}) => {
   return new Promise((resolve, reject) => {
     exec(
-      `python ${filepath} ${ inputPath }`,
+      `python ${filepath} -f ${ inputPath }`,
       (error, stdout, stderr) => {
         error && reject({ error, stderr });
         stderr && reject(stderr);
