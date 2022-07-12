4cec1c5331c9ad9abaa7aafd2dc25d7a7656f7e1
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2022-07-12 08:15:01 +0530

    new Api work started

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { generateFile } = require('./cppfileGenerator');
+const { inputFile }  = require('./inputfileGenerator') 
+const { fileExecutor  } = require('./cppfileExecutor');
+const app = express();
+
+
+app.use('*',cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+
+app.get('/run',(req,res) =>{
+    console.log(req.body);
+    res.json({data : "Hello world from the node Js"});
+});
+
+
+app.post('/compile', async (req,res) => {
+    
+    const {code ,language , input = "Hello"} = req.body;
+    const filepath = await generateFile( req.body );
+    const  inputPath = await inputFile({filepath , input});
+
+    let output = "compilation Error";
+    try
+    {
+        output = await fileExecutor({filepath , inputPath}); 
+    }
+    catch(error) {
+       console.log(error);
+    }
+    console.log(output);
+    res.send({output : output});
+});
+
+app.listen(PORT,() =>{
+    console.log(`server is started at the port http://localhost:${PORT}`)
+})
