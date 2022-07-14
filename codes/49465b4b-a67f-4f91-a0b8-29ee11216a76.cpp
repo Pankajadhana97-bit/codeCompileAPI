2c6fe75d9cee0110cc42c25c0dd97b436a26db24
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2022-07-15 01:35:01 +0530

    python is running

---
+++ codes/49465b4b-a67f-4f91-a0b8-29ee11216a76.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+
+  int m;
+  cin>>m;
+
+  cout << n*m <<endl;
+  return 0;
+}
--- executePy.js
+++ executePy.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 const executePy = async ({filepath,inputPath}) => {
   return new Promise((resolve, reject) => {
     exec(
-      `python ${filepath} -f ${ inputPath }`,
+      `python ${filepath} < ${ inputPath }`,
       (error, stdout, stderr) => {
         error && reject({ error, stderr });
         stderr && reject(stderr);
